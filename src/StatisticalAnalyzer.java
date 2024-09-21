b3800852ad5722127d6de61dc9d599915ad665a4
vm.baranov <vbaranovitaliy@gmail.com>
2024-09-21 15:58:03 +0300

    Done

--- src/StatisticalAnalyzer.java
+++
@@ -1,7 +0,0 @@
-public class StatisticalAnalyzer {
-    public int findMostLikelyShift(String encryptedText,
-                                   char[] alphabet, String representativeText) {
-        // Логика статистического анализа для определения сдвига
-        return 0;
-    }
-}
